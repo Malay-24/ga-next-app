39994c443175d3001bc5ce51c6a4c88575af2f60
Malay-24 <malaybajpai24@gmailcom>
2023-01-08 17:30:57 +0530

    add

--- pages/counter.jsx
+++ pages/counter.jsx
@@ -4,6 +4,7 @@ const Counter = () => {
     const [count,setCount]=React.useState(0)
   return (
     <div>
+        <h1>Counter</h1>
         <h1>Count:{count}</h1>
         <button  onClick={()=>setCount(count+2)}>INC</button>
         <button disabled={count===0} onClick={()=>setCount(count-1)}>DEC</button>
