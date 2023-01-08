a950c650c585c21b3f81ee1c1a427069fe3fdeb6
Malay-24 <malaybajpai24@gmailcom>
2023-01-08 16:33:35 +0530

    let

--- pages/counter.jsx
+++ pages/counter.jsx
@@ -5,7 +5,7 @@ const Counter = () => {
   return (
     <div>
         <h1>Count:={count}</h1>
-        <button disabled={count===0} onClick={()=>setCount(count+1)}>INC</button>
+        <button disabled={count===0} onClick={()=>setCount(count+2)}>INC</button>
         <button onClick={()=>setCount(count-1)}>DEC</button>
     </div>
   )
